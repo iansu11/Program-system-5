5c74f92a4af07e4c8a9bd49ecb93a486c8cbbc6e
iansu11 <twiansu@gmail.com>
2026-07-08 09:14:08 +0800

    Update db.js

--- js/db.js
+++ js/db.js
@@ -478,7 +478,11 @@ async function syncCategoryDeltaToCloud(catId, diff) {
             if (bankNameEl) bankNameEl.innerHTML = `<i class="fa-solid fa-folder-open" style="color: #60a5fa; margin-right: 8px;"></i> 目前題庫: ` + currentBankName;
                 
             saveToLocal(shouldSyncDb, false);      
-            window.location.href = '/categories';
+            if (!window.location.pathname.includes('workspace')) {
+                window.location.href = '/categories';
+            } else {
+                window.location.reload();
+            }
             checkForUpdates();
 
         } catch (err) { 
