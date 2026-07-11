258802d675e5237c255a30f734726f1483220743
iansu11 <twiansu@gmail.com>
2026-07-11 09:34:18 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -686,7 +686,7 @@ function hardResetAll() {
     }
 
 function saveBankOrder() {
-        const cards = document.querySelectorAll('#customBankList .bank-btn');
+        const cards = document.querySelectorAll('#customBankList .saas-card');
         const newOrder = [];
         cards.forEach(card => {
             const bank = db.customBanks.find(b => b.id === card.dataset.id);
