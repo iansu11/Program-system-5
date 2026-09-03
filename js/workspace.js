0b3d4724b2805f33cdea0e432cd97658fdd83fdd
iansu11 <twiansu@gmail.com>
2026-09-03 20:00:33 +0800

    Update workspace.js

--- js/workspace.js
+++ js/workspace.js
@@ -1141,10 +1141,7 @@ function enableTabInTextarea(id) {
         el.addEventListener('keydown', function(e) {
             if (e.key === 'Tab') { 
                 e.preventDefault(); 
-                const start = this.selectionStart; 
-                const end = this.selectionEnd; 
-                this.value = this.value.substring(0, start) + "    " + this.value.substring(end); 
-                this.selectionStart = this.selectionEnd = start + 4; 
+                this.setRangeText('    ', this.selectionStart, this.selectionEnd, 'end');
             }
         });
     }
