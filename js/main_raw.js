d08beff02f23996d2f3a62a549c0f2780d946850
iansu11 <twiansu@gmail.com>
2026-09-12 21:37:38 +0800

    Update main_raw.js

--- js/main_raw.js
+++ js/main_raw.js
@@ -1107,12 +1107,13 @@
     function enableTabInTextarea(id) {
         const el = document.getElementById(id); 
         if (!el) return;
-        if (el.dataset.tabEnabled) return;
-        el.dataset.tabEnabled = "true";
         el.addEventListener('keydown', function(e) {
             if (e.key === 'Tab') { 
                 e.preventDefault(); 
-                this.setRangeText('    ', this.selectionStart, this.selectionEnd, 'end');
+                const start = this.selectionStart; 
+                const end = this.selectionEnd; 
+                this.value = this.value.substring(0, start) + "    " + this.value.substring(end); 
+                this.selectionStart = this.selectionEnd = start + 4; 
             }
         });
     }
